855c28392144978afa3f26a53c3afd1aacac15f4
Adrián Rivero <adrianriverof@gmail.com>
2024-12-04 20:41:26 +0100

    test: theres score manager

--- scripts/score_tests.gd
+++ scripts/score_tests.gd
@@ -1,8 +1,16 @@
 extends GutTest
 
 
+var ScoreManager = load("res://scripts/score_manager.gd")
+
 
 func test_ok():
 	assert_true(true)
 	
+
+func test_theres_scoremanager():
+	var sut = ScoreManager.new()
+	
+	assert_not_null(sut)
+
 
