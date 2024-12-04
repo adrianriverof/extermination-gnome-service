a8ce04e724db4830b65eec81bee92b683c126292
Adrián Rivero <adrianriverof@gmail.com>
2024-12-04 21:13:56 +0100

    test: en combo si no pasa el combo time

--- scripts/score_tests.gd
+++ scripts/score_tests.gd
@@ -64,3 +64,14 @@ func test_fuera_de_combo_cuando_pasa_mas_del_combotime():
 	
 	
 	assert_eq(sut.in_combo(), false )
+
+func test_dentro_de_combo_cuando_no_pasa_mas_del_combotime():
+	
+	var sut = ScoreManager.new()
+	
+	sut.time_passed = 24
+	sut.matar_cuca()
+	sut.time_passed += sut.combo_time - 0.1
+	
+	
+	assert_eq(sut.in_combo(), true )
