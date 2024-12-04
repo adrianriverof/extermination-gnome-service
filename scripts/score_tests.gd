6d2ab7a503a7ad5ab0aede3c0abf312d6861e509
Adrián Rivero <adrianriverof@gmail.com>
2024-12-04 20:38:35 +0100

    test: tests work

--- scripts/score_tests.gd
+++ scripts/score_tests.gd
@@ -1,16 +1,8 @@
-extends Node
+extends GutTest
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+func test_ok():
+	assert_true(true)
+	
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
