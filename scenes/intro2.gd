d0b5ba9e5c00ccad143fb9cbbc29414ee876917e
adrianriverof <adrianriverof@gmail.com>
2024-12-07 12:07:22 +0100

    intro completa

--- scenes/intro2.gd
+++ scenes/intro2.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 onready var skiplabel = $INTROPLAYER/RichTextLabel
+onready var skiptimer = $SkipTextTimer
 
 func _ready():
 	skiplabel.visible = false
@@ -15,6 +16,10 @@ func _input(event):
 	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_cancel"):
 		if skiplabel.visible == false:
 			skiplabel.visible = true
-			
+			skiptimer.start()
 		else:
 			go_to_menu()
+
+
+func _on_SkipTextTimer_timeout():
+	skiplabel.visible = false
