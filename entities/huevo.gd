a8507ef8115327628f8774bb0fd274cf6e204d31
adrianriverof <adrianriverof@gmail.com>
2024-12-08 19:30:50 +0100

    huevo animaciones y colisiones

---
+++ entities/huevo.gd
@@ -0,0 +1,39 @@
+extends Spatial
+
+
+
+
+var damaged = false
+
+
+func _ready():
+	play_normal_animation()
+	
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	play_normal_animation()
+
+func damage():
+	$DamageCooldown.start()
+	if !damaged:
+		play_hitted_animation()
+		damaged = true
+	elif $DamageCooldown.is_stopped():
+		# dar puntos por golpear
+		pass
+	
+
+
+
+func play_normal_animation():
+	$AnimationPlayer.play("IDDLE001")
+
+func play_hitted_animation():
+	$AnimationPlayer.play("bumbum 1001")
+
+func play_ending_animation():
+	$AnimationPlayer.play("IDDLE003")
+
+
+func _on_Timer_timeout():
+	play_ending_animation()
