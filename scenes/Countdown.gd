c5a5d4a68e10d71dcd3066108d1312270f5cdfa7
adrianriverof <adrianriverof@gmail.com>
2024-12-09 22:34:37 +0100

    countdown

--- scenes/Countdown.gd
+++ scenes/Countdown.gd
@@ -6,11 +6,24 @@ extends CanvasLayer
 func _ready():
 	#Engine.time_scale = 0
 	get_tree().paused = true
+	
+	randomize()
+	var number = (randi() % 4 )
+	print(number)
+	$Gnometips.frame = number
+	
+#	for i in range(100):
+#		print(randi() % 4)
+	
+func shake_number():
+	$numbers/ShakeTween.start()
+
 
 func _on_Timer_timeout():
+	start_level()
+
+func start_level():
 	Engine.time_scale = 1
 	get_tree().paused = false
 	get_parent().start_level()
 	queue_free()
-
-
